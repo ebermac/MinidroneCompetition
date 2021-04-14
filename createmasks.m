dcbd3a6221c98bc71f63a459b29f99ef8e6f2865
ebermac <eber_macmar@hotmail.com>
2021-04-14 18:05:04 -0500

    Added createmasks.m for testing

---
+++ createmasks.m
@@ -0,0 +1,49 @@
+% bwmasktrapezoid
+xmask = [65 95 90 70];
+ymask = [0 0 50 50];
+bwmask1 = poly2mask(xmask,ymask,120,160);
+bwmasktrapezoid = boolean(bwmask1);
+imshow(bwmask1)
+
+
+% bwmaskpathtriangle
+xmask = [80 66 80];
+ymask = [20 20 60];
+bwmaskleft = boolean(poly2mask(xmask,ymask,120,160));
+bwmaskright = flip(bwmaskleft,2);
+
+bwmaskleft = int8(bwmaskleft);
+bwmaskright = int8(bwmaskright);
+bwmaskleft = bwmaskleft*(-1);
+
+bwmaskpathtriangle = imadd(bwmaskleft,bwmaskright);
+
+figure()
+imshow(bwmaskpathtriangle~=0)
+
+
+% bwmasktraveled
+xmask = [60 100 100 60];
+ymask = [0 0 120 120];
+bwmasktraveled = poly2mask(xmask,ymask,120,160);
+bwmasktraveled = ~boolean(bwmasktraveled);
+figure()
+imshow(bwmasktraveled)
+
+
+% create matrices
+angle = [180:-1:-180];
+for i = 1:size(angle,2)
+bwmasktrapezoidmatrix(:,:,i) = imrotate(bwmasktrapezoid, angle(i), 'bilinear', 'crop');
+bwmasktraveledmatrix(:,:,i) = imrotate(bwmasktraveled, angle(i), 'bilinear', 'crop');
+bwmaskpathtrianglematrix(:,:,i) = imrotate(bwmaskpathtriangle, angle(i), 'nearest', 'crop');
+end
+
+% angle = [180:-1:-180];
+% for i = 1:size(angle,2)
+% bwmaskpathtrianglematrix(:,:,i) = imrotate(bwmaskpathtriangle, angle(i), 'nearest', 'crop');
+% end
+
+bwmaskpathtrianglematrixmatlab = int8(bwmaskpathtrianglematrix);
+bwmasktrapezoidmatrixmatlab = uint8(bwmasktrapezoidmatrix);
+bwmasktraveledmatrixmatlab = uint8(bwmasktraveledmatrix);
